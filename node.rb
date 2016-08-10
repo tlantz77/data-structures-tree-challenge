4e66e37481fb54a7eaab3e0cbecf49601dcebac5
tlantz77 <tlantz@gmail.com>
2016-08-10 17:23:11 -0500

    implement tree nodes

--- node.rb
+++ node.rb
@@ -1,3 +1,14 @@
 class Node
 
+  attr_accessor :value, :children
+
+  def initialize(value)
+    @value = value
+    @children = []
+  end
+
+  def add_child(child)
+    @children << child
+  end
+
 end
--- node_spec.rb
+++ node_spec.rb
@@ -2,4 +2,24 @@ require_relative 'node'
 
 describe Node do
 
+  let(:parent) { Node.new("mom") }
+  let(:first_child) { Node.new("son") }
+  let(:second_child) { Node.new("daughter") }
+
+  it "has a value" do
+    expect(parent.value).to eq("mom")
+  end
+
+  it "is instantiated with empty children array" do
+    expect(parent.children.empty?).to be true
+  end
+
+  it "can add other nodes as children" do
+    parent.add_child(first_child)
+    parent.add_child(second_child)
+    expect(parent.children.length).to eq(2)
+    expect(parent.children[0].value).to eq("son")
+  end
+
+
 end
